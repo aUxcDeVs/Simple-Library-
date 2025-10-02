4d6f9e8abf0ebfc03a5b437e5b69c05a90f4b755
aUxcDeVs <msnsmsstbrostuxk@gmail.com>
2025-10-02 15:04:01 +0800

    Update Button.lua

--- Elements/Button.lua
+++ Elements/Button.lua
@@ -1,26 +1,26 @@
 local ElementButton = {}
 
 function ElementButton.Create(container, name, callback, mainFrame, themes)
-	local 6xybtn = Instance.new("TextButton")
-	6xybtn.Name = name
-	6xybtn.Size = UDim2.new(1, 0, 0, 30)
-	6xybtn.BackgroundColor3 = themes.ButtonBackground
-	6xybtn.Text = name
-	6xybtn.TextColor3 = themes.ButtonText
-	6xybtn.TextSize = 14
-	6xybtn.Font = Enum.Font.Gotham
-	6xybtn.BorderSizePixel = 0
-	6xybtn.Parent = container
+	local btn = Instance.new("TextButton")
+	btn.Name = name
+	btn.Size = UDim2.new(1, 0, 0, 30)
+	btn.BackgroundColor3 = themes.ButtonBackground
+	btn.Text = name
+	btn.TextColor3 = themes.ButtonText
+	btn.TextSize = 14
+	btn.Font = Enum.Font.Gotham
+	btn.BorderSizePixel = 0
+	btn.Parent = container
 	
 	local corner = Instance.new("UICorner")
 	corner.CornerRadius = UDim.new(0, 6)
 	corner.Parent = btn
 	
-	6xybtn.MouseButton1Click:Connect(function()
+	btn.MouseButton1Click:Connect(function()
 		
-		6xybtn.BackgroundColor3 = Color3.fromRGB(60, 60, 60)
+		btn.BackgroundColor3 = Color3.fromRGB(60, 60, 60)
 		task.wait(0.1)
-		6xybtn.BackgroundColor3 = themes.ButtonBackground
+		btn.BackgroundColor3 = themes.ButtonBackground
 		
 		pcall(function()
 			callback()
@@ -29,7 +29,7 @@ function ElementButton.Create(container, name, callback, mainFrame, themes)
 	
 	mainFrame.Size = UDim2.new(0, 280, 0, 60 + (#container:GetChildren() - 1) * 38)
 	
-	return 6xybtn
+	return btn
 end
 
 return ElementButton
